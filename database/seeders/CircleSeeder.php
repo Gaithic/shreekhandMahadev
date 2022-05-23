fcd6ab76cca1f874f0d9f347bc54694a9c160643
Rohit gautam <rohitgautamofficialmail@gmail.com>
2022-05-23 12:13:34 +0530

    Seder and validation

--- database/seeders/CircleSeeder.php
+++ database/seeders/CircleSeeder.php
@@ -18,91 +18,103 @@ class CircleSeeder extends Seeder
         DB::table('circles')->insert([
             'id' => 1,
             'circleName' => 'Bilaspur',
-            
-        
+            'district_id'  => 1
+
+
         ]);
 
-        
+
         DB::table('circles')->insert([
             'id' => 2,
             'circleName' => 'Mandi',
-            
-        
+            'district_id'  => 2
+
+
         ]);
 
-        
+
         DB::table('circles')->insert([
             'id' => 3,
             'circleName' => 'Kullu',
-            
-        
+            'district_id'  => 3
+
+
         ]);
 
         DB::table('circles')->insert([
             'id' => 4,
             'circleName' => 'Kullu',
-            
-        
+            'district_id'  => 4
+
+
         ]);
 
 
-        
+
         DB::table('circles')->insert([
             'id' => 5,
             'circleName' => 'Rampur',
-            
+            'district_id'  => 3
+
         ]);
 
         DB::table('circles')->insert([
             'id' => 6,
             'circleName' => 'Rampur',
-            
+            'district_id'  => 5
+
         ]);
 
-        
+
         DB::table('circles')->insert([
             'id' => 7,
             'circleName' => 'Rampur',
-            
+            'district_id'  => 6
+
         ]);
 
         DB::table('circles')->insert([
             'id' => 8,
             'circleName' => 'Shimla',
-            
-           
+            'district_id'  => 5
+
+
         ]);
 
-        
+
         DB::table('circles')->insert([
             'id' => 9,
             'circleName' => 'GHNP',
-            
-           
+            'district_id'  => 3
+
+
         ]);
 
 
-        
+
         DB::table('circles')->insert([
             'id' => 10,
             'circleName' => 'Shimla(WL)',
-            
-           
+            'district_id'  => 4
+
+
         ]);
 
-        
+
         DB::table('circles')->insert([
             'id' => 11,
             'circleName' => 'Dharmshala',
-            
-            
+            'district_id'  => 7
+
+
         ]);
 
-        
+
         DB::table('circles')->insert([
             'id' => 12,
             'circleName' => 'Hamirpur',
-            
+            'district_id'  => 7
+
         ]);
     }
 }
--- public/asset/users/registerFormValidation.js
+++ public/asset/users/registerFormValidation.js
@@ -1,74 +1,3 @@
-function dobValidate(birth) {
-
-
-    var today = new Date();
-    var nowYear = today.getFullYear();
-    var nowMonth = today.getMonth();
-    var nowDay = today.getDate();
-    var birthDay = document.getElementById('date_of_birth').value;
-
-    var birth = new Date(birthDay);
-    var birthYear = birth.getFullYear();
-    var birthMonth = birth.getMonth();
-    var birthday = birth.getDate();
-    var age = nowYear - birthYear;
-    var age_month = nowMonth - birthMonth;
-    var age_day = nowDay - birthday;
-
-    if (age > 100) {
-        document.getElementById('dobError').innerHTML= "Age cannot be more than 100 Years.Please enter correct age";
-        return false;
-    }
-    if (age_month < 0 || (age_month == 0 && age_day < 0)) {
-        age = parseInt(age) - 1;
-        return false;
-
-    }
-    if ((age == 18 && age_month <= 0 && age_day <= 0) || age < 18) {
-        document.getElementById('dobError').innerHTML= "Age should be more than 18 years.Please enter a valid Date of Birth";
-        return false;
-    }
-
-}
-
-function validDiff(){
-    const birthDay = document.getElementById('date_of_birth').value;
-    const dateOfJoin = document.getElementById('date_of_joining').value;
-
-    // var today = new Date();
-    // var theYear = today.getFullYear();
-    // var theMonth = today.getMonth();
-    // var theDay  = today.getDay();
-    // var todayDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-
-    var YearOfBirth = new Date(birthDay);
-    var YearOfJoin = new Date(dateOfJoin);
-
-    var getYearOfBirth = YearOfBirth.getFullYear();
-    var getYearOfJoin  = YearOfJoin.getFullYear();
-    var getYearDifference = getYearOfJoin - getYearOfBirth;
-
-    if(getYearDifference<=18){
-        document.getElementById('dojError').innerHTML= "Age and date of joining difference should be greater than 18!!";
-        console.log(todayDate);
-        console.log(dateOfJoin);
-        return false;
-    }
-
-    if(dateOfJoin>getYearDifference){
-        document.getElementById('dojError').innerHTML= "Joining Date not be greater than Today!!";
-        return false;
-    }
-
-
-
-
-}
-
-
-
-
-
 function validateForm(){
     const name = document.getElementById('name').value;
     const designation = document.getElementById('designation').value;
@@ -87,136 +16,122 @@ function validateForm(){
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     const cPassword = document.getElementById('confirm_password').value;
-    // const diffrence = Math(date_of_joining - date_of_birth);
+    const hackSafe = /[\\\/:*?<>|]+/;
+    var ifString = /^[A-Za-z ]+$/;
+    var ifNumber = /[1-9]/;
 
+    //name input validation
 
     if(name==''){
         document.getElementById('nameError').innerHTML= "Name is required";
         return false;
-    }
-    if(!isNaN(name)){
-        document.getElementById('nameError').innerHTML= "Only Words are allowed";
-        return false;
+    }else{
+        if(hackSafe.test(name)){
+            document.getElementById('nameError').innerHTML= "Special Character's are not allowed";
+            return false;
+        }else{
+            if(!ifString.test(name)){
+                document.getElementById('nameError').innerHTML= "Name Contain word's Only not number's";
+            }else{
+                document.getElementById('nameError').innerHTML= "";
+
+            }
+        }
     }
 
+    ///designation input validation
 
     if(designation==''){
         document.getElementById('designationError').innerHTML= "Designation is required";
         return false;
+    }else{
+        if(hackSafe.test(designation)){
+            document.getElementById('designationError').innerHTML= "Special Character's are not allowed";
+            return false;
+        }else{
+            if(!ifString.test(designation)){
+                document.getElementById('designationError').innerHTML= "Enter Valid Designation Name!!";
+            }else{
+                document.getElementById('designationError').innerHTML= "";
+            }
+
+        }
     }
 
-    if(!isNaN(designation)){
-        document.getElementById('designationError').innerHTML= "Only Words are allowed";
-        return false;
-    }
 
+    /// date_of_birth date input validation
 
-
-    if(date_of_birth==''){
+    if(date_of_birth==""){
         document.getElementById('dobError').innerHTML= "Date Of Birth is required";
         return false;
+    }else{
+        var today = new Date();
+        var theYear = today.getFullYear();
+        var theMonth  = today.getMonth();
+        var theDate = today.getDate();
+
+        var userInputYear = new Date(date_of_birth)
+        var inputYear = userInputYear.getFullYear();
+        var inputMonth = userInputYear.getMonth();
+        var inputDate  = userInputYear.getDate();
+
+        var differenceAge = theYear - inputYear
+
+        if(differenceAge > 100){
+            document.getElementById('dobError').innerHTML= "Age not be grater than 100 Years!!";
+            return false;
+        }else{
+            if(differenceAge <=18){
+                document.getElementById('dobError').innerHTML= "Age should be grater than 18 year!!";
+                return false;
+            }else{
+                document.getElementById('dobError').innerHTML= "";
+            }
+
+        }
     }
 
-    if(dobValidate(date_of_birth)){
-        document.getElementById('dobError').innerHTML= "Date Of Birth is required";
-        return false;
-    }
+
+    // date_of_joining input validation
 
     if(date_of_joining==''){
         document.getElementById('dojError').innerHTML= "Date of Joining is required";
         return false;
+    }else{
+        if(date_of_joining <= date_of_birth){
+            document.getElementById('dojError').innerHTML= "Date of Joining is is not less than or equal to the Date of Birth!!";
+            return false;
+        }else{
+            var birthDay = new Date(date_of_birth)
+            var userJoiningYear = new Date(date_of_joining)
+            var birthDayFullYear = birthDay.getFullYear();
+            var getJoiningYear = userJoiningYear.getFullYear();
+            var getDifference = getJoiningYear - birthDayFullYear;
+
+            if(getDifference<18){
+                document.getElementById('dojError').innerHTML= "Age Difference Between Date of birth and Date of Joining Must be equal to grater than 18 Years!! ";
+                return false;
+            }else{
+                var todayDate = new Date();
+                var getTodayFullDate = todayDate.getFullYear()+'-'+('0' + (todayDate.getMonth()+1)).slice(-2)+'-'+todayDate.getDate();
+                if(date_of_joining>getTodayFullDate){
+                    document.getElementById('dojError').innerHTML= "Date not be grater than today!!";
+                    return false;
+                }else{
+                    document.getElementById('dojError').innerHTML= "";
+                }
+            }
+        }
     }
 
-    if(date_of_joining <= date_of_birth){
-        document.getElementById('dojError').innerHTML= "Date of Joining and date of birth not be equal";
-        return false;
-    }
-
-    if(validDiff(date_of_joining)){
-
-    }
-
-    if(office_address==''){
+    // input validation for office_address input field
+    if(office==''){
         document.getElementById('officeError').innerHTML= "Office Address is required";
         return false;
-    }
-
-    if(district==''){
-        document.getElementById('districtError').innerHTML= "District Name is required";
-        return false;
-    }
-
-    if(circle==''){
-        document.getElementById('circleError').innerHTML= "District Name is required";
-        return false;
-    }
-
-
-    if(division==''){
-        document.getElementById('divisionError').innerHTML= "District Name is required";
-        return false;
-    }
-
-
-    if(range==''){
-        document.getElementById('rangeError').innerHTML= "Office Name is required";
-        return false;
-    }
-
-
-    if(office_address==''){
-        document.getElementById('addressError').innerHTML= "Office Name is required";
-        return false;
-    }
-
-
-
-    if(gender==''){
-        document.getElementById('genderError').innerHTML= "Gender is required";
-        return false;
-    }
-
-    if(qualification==''){
-        document.getElementById('qualificationError').innerHTML= "Qualification field is required";
-        return false;
+    }else{
+        document.getElementById('officeError').innerHTML= "";
 
     }
-
-
-    if(email==''){
-        document.getElementById('emailError').innerHTML= "Email is required";
-        return false;
-
-    }
-
-
-    if(contact==''){
-        document.getElementById('contactError').innerHTML= "Contact is required";
-        return false;
-    }
-
-
-    if(username==''){
-        document.getElementById('usernameError').innerHTML= "Username is required ";
-        return false;
-    }
-
-    if(password==''){
-        document.getElementById('passwordError').innerHTML= "Password is required";
-        return false;
-    }
-
-    if(confirm_password==''){
-        document.getElementById('cPasswordError').innerHTML= "Password is required";
-        return false;
-    }
-
-    if(password!=confirm_password){
-        document.getElementById('cPasswordError').innerHTML= "Password is Incorrect";
-        return false;
-    }
-
-
 
 
 
